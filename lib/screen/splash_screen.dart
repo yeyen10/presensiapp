835a50eaa17fbfda18d82ef14b0f9d0d37e7bc4f
Vynixxx <yeyenyulastrisitorus@gmail.com>
2024-06-12 22:16:07 +0700

    preen

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:presensi_yeyens/screen/login_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 3)).then((value) {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(
+            builder: (context) => const LoginScreen(),
+          ),
+          (route) => false);
+    });
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo_polbeng.png',
+              width: 128,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text('PresensiApp',
+                style: GoogleFonts.manrope(
+                  fontSize: 34,
+                  color: const Color(0xFF12A3DA),
+                  fontWeight: FontWeight.w700,
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
